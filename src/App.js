a3510aa8ab3db38c5eea3c3d2899f092234351dc
vezaadiloviana <vezaadiloviana27@gmail.com>
2023-06-24 10:38:35 +0700

    function adding data

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ class App extends React.Component {
     })
   }
 
+  addTask = data => {
+    const id = this.state.todos.length
+    const newData = {
+      id: id + 1,
+      title: data
+    }
+    this.setState({
+      todos: [...this.state.todos, newData]
+    })
+  } 
+
   render() {
     const { todos } = this.state;
     return (
@@ -38,7 +49,7 @@ class App extends React.Component {
           )}
         </div>
         <div>
-          <FormInput />
+          <FormInput add={this.addTask}/>
         </div>
       </div>
     );
--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -3,11 +3,26 @@ import Button from "./Button";
 import "../assets/css/formInput.css"
 
 class FormInput extends React.Component {
+    state = {
+        text: ""
+    }
+    change = e => {
+        this.setState({ text: e.target.value})
+    }
+    submit = e => {
+        e.preventDefault()
+        if(this.state.text !== ""){
+            this.props.add(this.state.text)
+        }
+        this.setState({
+            text: ""
+        })
+    }
     render() { 
         return ( 
-            <form style={inputForm}>
-                <input style={input} type="text" placeholder="add task"/>
-                <Button text="Add" variant="primary" />
+            <form style={inputForm} onSubmit={this.submit}>
+                <input onChange={this.change} style={input} type="text" placeholder="add task" value={this.state.text}/>
+                <Button text="Add" variant="primary" action={this.submit}/>
             </form>
          );
     }
